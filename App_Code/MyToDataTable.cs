3f201df54484c8cda8a24e0649f0a4ef64e3cda6
voravut_somb <voravut.somboornpong@thaiunion.com>
2024-02-23 11:41:52 +0700

    toCSV

--- App_Code/MyToDataTable.cs
+++ App_Code/MyToDataTable.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Web;
@@ -26,6 +27,45 @@ public static class MyToDataTable
             table.Rows.Add(row);
         }
         return table;
+    }
+    public static void ToCSV(DataTable dtDataTable, string strFilePath)
+    {
+        StreamWriter sw = new StreamWriter(strFilePath, false);
+        //headers
+        for (int i = 0; i < dtDataTable.Columns.Count; i++)
+        {
+            sw.Write(dtDataTable.Columns[i]);
+            if (i < dtDataTable.Columns.Count - 1)
+            {
+                sw.Write(",");
+            }
+        }
+        sw.Write(sw.NewLine);
+        foreach (DataRow dr in dtDataTable.Rows)
+        {
+            for (int i = 0; i < dtDataTable.Columns.Count; i++)
+            {
+                if (!Convert.IsDBNull(dr[i]))
+                {
+                    string value = dr[i].ToString();
+                    if (value.Contains(','))
+                    {
+                        value = String.Format("\"{0}\"", value);
+                        sw.Write(value);
+                    }
+                    else
+                    {
+                        sw.Write(dr[i].ToString());
+                    }
+                }
+                if (i < dtDataTable.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+        }
+        sw.Close();
     }
     public static String getColumnNameFromIndex(int column)
     {
